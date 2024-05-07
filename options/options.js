6515a666f0ca73e36cb062bfd2effa34899e2603
Crowdbotics <team@crowdbotics.com>
2024-05-07 14:09:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-poetry-47837",
+  url: "https://red-poetry-47837.botics.co",
+  api: "https://red-poetry-47837.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
